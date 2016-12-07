c3dae7d6523afe3813b3be52c8f5e5fdf9235789
Norbert Glen <norbertglen7@gmail.com>
2016-12-07 05:24:02 +0300

    Add cancel button to modals

--- modules/ui/Modal/Modal.jsx
+++ modules/ui/Modal/Modal.jsx
@@ -133,8 +133,9 @@ export default Modal = React.createClass( {
             queue.map(function(q,idx){
               return <div className={"modal-dialog"+(q.size?(" modal-dialog-"+q.size):"")} key={idx}>
                 <div className="modal-body">
+                <span style={{float:"right",zIndex:4000,cursor:"pointer",fontSize:"20px",color:"#999",width:"20px"}} data-dismiss="modal">&times;</span>
                   {q.content}
-                  {/*<span style={{position:"absolute",right:"10px",top:"35px",zIndex:4000,cursor:"pointer",fontSize:"20px",color:"#999",width:"20px"}} data-dismiss="modal">&times;</span>*/}
+                  
                 </div>
               </div>
             })
