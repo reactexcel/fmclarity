1c345a23a158a92ba7fe395f0958e21711bd57b1
Leo Keith <leo@fmclarity.com>
2016-12-07 17:46:03 +1000

    Some tweaks for full screen modal on screens < 768px

--- modules/ui/MaterialNavigation/imports/components/TopNavigationBar.jsx
+++ modules/ui/MaterialNavigation/imports/components/TopNavigationBar.jsx
@@ -35,7 +35,7 @@ export default function TopNavigationBar( props ) {
 
     return (
         <div className="top-navigation-bar">
-            <div className="sidebar-back-screen" onClick = {toggleLeftSideBar }></div>
+            <div className="sidebar-back-screen" onClick = { toggleLeftSideBar }></div>
             <nav className="nav-bar">
                 <div className="icon-region-1">
                     <span id="nav-menu-icon" className="topnav-icon" onClick = { toggleLeftSideBar }><i className="fa fa-bars"></i></span>
@@ -55,17 +55,16 @@ export default function TopNavigationBar( props ) {
                             <i className="fa fa-bell"></i>
                             <div style={{float:"right",width:"15px"}}>
                                 { notifications && notifications.length ?
-                                    <span className="label label-notification">{notifications.length}</span>
+                                    <span className="label label-notification">{ notifications.length }</span>
                                 : null }
                             </div>
                         </span>
-                        <NotificationList items={notifications}/>
-                        <DesktopNotificationPopUp {...props} />
+                        <NotificationList items = { notifications }/>
+                        <DesktopNotificationPopUp { ...props } />
                     </div>
                 </div>
             </nav>
         </div>
-
     )
 }
 
