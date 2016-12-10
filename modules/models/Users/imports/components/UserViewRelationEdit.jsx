0d8fe2bdf25605a376081190b5df146a12f81b25
anurag <arun.etech2011@gmail.com>
2016-12-10 15:23:23 +0530

    [FWA-817]Done!-When a new member is created from add new tenant function, default member type should be tenant

--- modules/models/Users/imports/components/UserViewRelationEdit.jsx
+++ modules/models/Users/imports/components/UserViewRelationEdit.jsx
@@ -6,6 +6,8 @@
 import React from "react";
 import { Select } from '/modules/ui/MaterialInputs';
 
+import { Users } from '/modules/models/Users';
+
 // so this should perhaps be included in the docmembers package??
 export default UserViewRelationEdit = React.createClass( {
 
@@ -14,6 +16,13 @@ export default UserViewRelationEdit = React.createClass( {
 		member = this.props.member;
 		group = this.props.group;
 		group.setMemberRole( member, role );
+		if ( _.contains([ "tenant", "resident" ], role) ) {
+			Users.update( { _id: member._id}, { $set: { role: role } } );
+			this.props.team.setMemberRole( member, role );
+			if (this.props.onChange) {
+				this.props.onChange();
+			}
+		}
 	},
 
 	render() {
