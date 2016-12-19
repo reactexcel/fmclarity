10f0ebeccdab58a6d4f58030d705a46ed41a57b4
Norbert Glen <norbertglen7@gmail.com>
2016-12-19 12:58:36 +0300

    Insert WO data that relates to request breakdown graph

---
+++ modules/mixins/Services/imports/components/ServicesRequestsViewRow.jsx
@@ -0,0 +1,48 @@
+/**
+ * @author          Leo Keith <leo@fmclarity.com>
+ * @copyright       2016 FM Clarity Pty Ltd.
+ */
+
+import React from 'react';
+
+/**
+ * @class 			ServicesProvidedEditorRow
+ * @memberOf 		module:mixins/Services
+ */
+function ServicesRequestsViewRow( props ) {
+
+	
+
+	service = props.service;
+	clickExpand = props.clickExpand;
+	var onChange = props.onChange;
+	readOnly = props.readOnly;
+
+	return (
+		<div>
+			<div className="services-editor-col services-editor-col-service">
+
+				{ clickExpand ?
+
+					<span
+						onClick 	= { clickExpand }
+						className 	= "services-editor-expand-icon"
+					>
+						<i className="fa fmc-fa-icon-expand"></i>
+					</span>
+
+				: null }
+
+				<input
+					defaultValue 	= { service || undefined }
+					readOnly 		= { readOnly }
+				/>
+
+			</div>
+
+		</div>
+	)
+
+}
+
+export default ServicesRequestsViewRow;
