96bbd83f06829f8ce09a19b9760528541d54b84e
anurag <arun.etech2011@gmail.com>
2016-12-27 10:36:23 +0530

    [FWA-859]-Remove 'facility manager' from the access list for 'destroy document'.

--- client/main.js
+++ client/main.js
@@ -244,7 +244,7 @@ Actions.addAccessRule( {
 	action: [
 		'destroy document'
 	],
-	role: [ 'facility manager', 'fmc support', 'portfolio manager' ],
+	role: [ 'fmc support', 'portfolio manager' ],
 	rule: { alert: true }
 } )
 
