0a1d5f696e1216c3995514103d65e7a55933ae33
Norbert Glen <norbertglen7@gmil.com>
2017-02-23 17:12:50 +0300

    FWA-379 Completed work orders should show attendance and completion time in comments

--- modules/models/Messages/imports/components/MessageView.jsx
+++ modules/models/Messages/imports/components/MessageView.jsx
@@ -130,7 +130,12 @@ export default MessageView = React.createClass( {
 
                         <div className="message-footer">
                             <small className="text-muted">{moment(createdAt).format('MMM Do YYYY, h:mm:ss a')}</small>
-                            {message.verb=='completed' ? <small className="text-muted pull-right">Attended - {moment(message.getTarget().closeDetails.completionDate).format('MMM Do YYYY, h:mm:ss a')}</small> : null}
+                            {message.verb=='completed' ? 
+                            <div>
+                            <small className="text-muted pull-right">Attended - {moment(message.getTarget().closeDetails.attendanceDate).format('MMM Do YYYY, h:mm:ss a')}</small><br />
+                            <small className="text-muted pull-right">Completed - {moment(message.getTarget().closeDetails.completionDate).format('MMM Do YYYY, h:mm:ss a')}</small>
+                            </div>
+                             : null}
                         </div>
                     </div>
                 </div>
