e498bbcb4833fc1a25620b9457893a3f0c19fa3e
Norbert Glen <norbertglen7@gmail.com>
2017-04-06 12:50:22 +0300

    FWA-1160 Highlight pale-red color the rows of expired documents in the documents section listing.

--- modules/models/Documents/imports/components/DocIconHeader.jsx
+++ modules/models/Documents/imports/components/DocIconHeader.jsx
@@ -7,6 +7,17 @@ import React from "react";
 
 export default function DocIconHeader( props ) {
 	return (
+		<div>
+		<table className="pull-right" style={{fontStyle:"italic"}}>
+            <tr>
+                <td><div style={{height:"18px",width:"18px",background:"#ffffcc"}}></div></td>
+                <td style = {{ color: "rgb(153, 153, 153)" }}>Expires within 2 weeks</td>
+            </tr>
+            <tr>
+                <td><div style={{height:"18px",width:"18px",background:"#FFE0E0"}}></div></td>
+                <td style = {{ color: "rgb(153, 153, 153)" }}>Expired Document</td>
+            </tr>
+        </table>
 		<div style={{padding:"14px 24px 14px 24px",borderBottom:"1px solid #ddd",backgroundColor:"#eee",fontWeight:"bold"}}>
 			<span style={{display:"inline-block",minWidth:"18px",paddingRight:"24px"}}>&nbsp;</span>
 			<span style={{display:"inline-block",width:"20%",minWidth:"20px"}}>Type</span>
@@ -15,5 +26,6 @@ export default function DocIconHeader( props ) {
 			{/*<span style={{display:"inline-block",width:"9%",minWidth:"20px",paddingLeft:"10px"}}>Work order link</span>*/}
 			<span style={{display:"inline-block",width:"1%",minWidth:"20px",paddingLeft:"10px"}}></span>
 		</div>
+        </div>
 	)
 }
