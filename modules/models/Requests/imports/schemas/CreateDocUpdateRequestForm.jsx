945aad77a02e598ad093a649b5a102190b6c5e0d
Norbert Glen <norbertglen7@gmail.com>
2017-04-15 19:09:36 +0300

    FWA-1128 modifications to document update request

---
+++ modules/models/Requests/imports/schemas/CreateDocUpdateRequestForm.jsx
@@ -0,0 +1,28 @@
+export default CreateDocUpdateRequestForm = [
+	/*'team',*/
+	'type',
+
+	'facility',
+
+	'name',
+
+	'service',
+	'subservice',
+	'supplier',
+
+	'supplierContact',
+
+	'assignee',
+
+	'priority',
+	'dueDate',
+
+	'frequency',
+	'duration',
+
+	'description',
+
+	'attachments',
+	'footer'
+
+];
