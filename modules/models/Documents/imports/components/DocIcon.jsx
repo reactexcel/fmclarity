22f9f4c5f75b7af049f54a079d92547a80264c65
Norbert Glen <norbertglen7@gmail.com>
2017-04-20 00:44:42 +0300

    FWA-1128 Resolve documents table formatting problem

--- modules/models/Documents/imports/components/DocIcon.jsx
+++ modules/models/Documents/imports/components/DocIcon.jsx
@@ -76,10 +76,12 @@ export default function DocIcon( props ) {
 			<span style={{display:"inline-block",width:"90%",minWidth:"20px",fontStyle:"italic"}}>Add document</span>
 		</div>
         <table className="doc-icon pull-right" style={{fontStyle:"italic"}}>
+            <tbody>
             <tr>
                 <td><div style={{height:"18px",width:"18px",background:"#ffffcc"}}></div></td>
                 <td style = {{ color: "rgb(153, 153, 153)" }}>Expires within 2 weeks</td>
             </tr>
+            </tbody>
         </table>
         </div>
         )
