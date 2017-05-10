a7fa9aa9a8f33e28c0ea1fa825fcac962444b790
anurag <arun.etech2011@gmail.com>
2017-05-10 13:14:21 +0530

    work on page layout

--- modules/features/Reports/imports/reports/MBMBuildingServiceReport.jsx
+++ modules/features/Reports/imports/reports/MBMBuildingServiceReport.jsx
@@ -87,6 +87,12 @@ const MBMBuildingServiceReport = React.createClass( {
 			ready: handle.ready()
 		}
 	},
+	componentWillMount(){
+		$("#fab").hide();
+	},
+	componentWillUnmount(){
+		$("#fab").show();
+	},
 
 	printChart(){
 		var component = this;
@@ -183,7 +189,7 @@ const MBMBuildingServiceReport = React.createClass( {
 					<h2>Building Service Requests {facility&&facility.name?" for "+facility.name: (facilities && facilities.length=='1') ? "for "+ facilities[0].name : " for all facilities"}</h2>
 				</div>
 				<div className="ibox-content">
-					<div style={{width:"630px","height":"300px",paddingLeft:"20%"}}>
+					<div style={{width:"830px","height":"450px",paddingLeft:"20%",paddingTop:"8%"}}>
 						<canvas id="bar-chart"></canvas>
 					</div>
 				</div>
@@ -306,12 +312,12 @@ const SingleServiceRequest = React.createClass( {
 	render() {
 		let data = this.getData();
 		return (
-			<div style={ { marginTop: "55px", marginBottom: "10px", borderTop:"2px solid"  } }>
+			<div style={ { marginTop: "100px", marginBottom: "10px", borderTop:"2px solid"  } }>
 				<div className="ibox-title">
 					<h2>Requests for {this.props.serviceName}</h2>
 				</div>
 				<div className="ibox-content">
-					<div style={{width:"630px","height":"300px",paddingLeft:"20%"}}>
+					<div style={{width:"830px","height":"400px",paddingLeft:"20%",paddingTop:"5%"}}>
 						<canvas id={"bar-chart-" + this.props.id} style={{width:"630px","height":"300px"}}></canvas>
 					</div>
 				</div>
@@ -320,7 +326,7 @@ const SingleServiceRequest = React.createClass( {
 						<DataTable items={data.length ? data : [{name:""}]} fields={this.fields} includeActionMenu={true} setDataSet={this.setDataSet}/>
 					</div>
 				</div>
-				<div style={ { marginTop: "5px", marginBottom: "-15px" } }>
+				<div style={ { marginTop: "20px", marginBottom: "-15px",height:"100px" } }>
 					<div className="comment-header">
 						<h4>Comments</h4>
 						<span style={{float: "right"}}>
@@ -347,6 +353,7 @@ const SingleServiceRequest = React.createClass( {
 						{this.state.showEditor?
 							<TextArea
 								ref="textarea"
+								style={{height:"50px"}}
 								value={this.state.comment}
 								onChange={( value ) => {this.setState({ comment: value })}}
 								/>:
