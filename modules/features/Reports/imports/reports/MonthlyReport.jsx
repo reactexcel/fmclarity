4aadb613095ab9ebfd5ed5d758690fc7511932bb
anurag <arun.etech2011@gmail.com>
2017-06-02 19:17:48 +0530

    work on monthly reprt header

--- modules/features/Reports/imports/reports/MonthlyReport.jsx
+++ modules/features/Reports/imports/reports/MonthlyReport.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDom from "react-dom";
 import { ReactMeteorData } from 'meteor/react-meteor-data';
 import { Facilities } from '/modules/models/Facilities';
+import { Files } from '/modules/models/Files';
 import DocViewEdit from '../../../.././models/Documents/imports/components/DocViewEdit.jsx';
 import moment from 'moment';
 import Reports from '../Reports.js';
@@ -16,6 +17,7 @@ export default MonthlyReport = React.createClass( {
 		let	user = Meteor.user();
 		let team = user.getSelectedTeam();
 		let facility = Session.getSelectedFacility();
+		console.log(facility);
 
 		return ( {
 			expandall: false,
@@ -63,8 +65,31 @@ export default MonthlyReport = React.createClass( {
 			} );
 		},200);
 	},
+	getImage( _id,facility ){
+		if(_id != null){
+			let address = facility.address
+			let file = Files.findOne({_id});
+			let url = null;
+			let { removedImg } = this.state;
+			if (file != null ) {
+				url = file.url();
+				if( _.contains(["jpg", "png"], file.extension()) && !_.contains(removedImg, _id) ) {
+					return (
+						<div  key={_id}>
+							<div style= {{fontSize:"30px",fontWeight:"500",textAlign:"center",marginTop:"3%"}}>Monthly Report ({moment().format('MMMM YYYY')}) For {facility.name}</div>
+							<div style={{margin:"26% 0 4% 0",fontSize:"25px",fontWeight:"500",textAlign:"center",color:"darkgrey"}}>{address.streetNumber +" " + address.streetName +" " + address.streetType +"," + address.city +" " + address.state +" " + address.postcode}</div>
+							<div style={{margin:"0 0 50% 17%"}}><img src={url} style={{ height:"30%", width:"80%" }} /></div>
+						</div>
+					);
+				}
+			}
+			return null;
+		}
+	},
 
 	render() {
+		let facility = this.state.facility;
+		let imgThumb = facility.thumb.hasOwnProperty("_id") ? facility.thumb._id : null
 		return (
 			<div>
 				<div>
@@ -75,6 +100,10 @@ export default MonthlyReport = React.createClass( {
 						Archive
 					</button>
 				</div>
+				<div style = {{border:"1px solid black"}}>
+					{this.getImage(imgThumb,facility)}
+				</div>
+			<div>
 			<div style={{paddingBottom:"10%"}}>
 				<MBMReport MonthlyReport/>
 			</div>
@@ -86,6 +115,7 @@ export default MonthlyReport = React.createClass( {
 				<MBMServiceImages MonthlyReport/>
 			</div>
 		</div>
+	</div>
 		)
 	}
 } )
