e5f4215ec7351eb3ff85a0bd080718e4930b9009
Abhishek singh <abhishekex2011@gmail.com>
2017-06-12 13:42:56 +0530

    [FWA-1137]-Status report title fixing

--- modules/features/Reports/imports/reports/RequestsStatusReport.jsx
+++ modules/features/Reports/imports/reports/RequestsStatusReport.jsx
@@ -35,6 +35,14 @@ const RequestsStatusReport = React.createClass( {
 		}
 	},
 
+	componentDidMount(){
+		$('.nav-list-selected').css('display','none')
+	},
+
+	componentWillUnmount(){
+		$('.nav-list-selected').css('display','block')
+	},
+
 	getMeteorData() {
 
 		var user, team, facility, requests, data = {};
@@ -42,7 +50,7 @@ const RequestsStatusReport = React.createClass( {
 		if ( user ) {
 			var q = {};
 			team = user.getSelectedTeam();
-			facility = this.state.facility || user.getSelectedFacility();
+			facility = this.state.facility;
 			service = this.state.service;
 			supplier = this.state.supplier;
 			if ( facility ) {
@@ -191,10 +199,10 @@ const RequestsStatusReport = React.createClass( {
 
 		let { team, showFacilityName } = this.data, { facility, service } = this.state;
 		let fields = showFacilityName ? this.fields : _.omit( this.fields, "Facility" );
-		let styleForPDF = '<style type="text/css" media="print">.table {border-top: 2px solid black;border-bottom: 2px solid black;border-left: 2px solid black;border-right: 2px solid black;} #pre-head {border-right:2px solid black;text-align:center;border-bottom: 2px solid black;} #last-head {text-align:center;border-bottom: 2px solid black;} #pre-col {border-right:1px solid black; border-bottom:1px solid black} .Summary{min-width:320px;} .Supplier{min-width:120px;} .Service{min-width:200px;} #last-col {border-bottom:1px solid black}</style>';
+		let styleForPDF = '<style type="text/css" media="print">.table {border-top: 2px solid black;border-bottom: 2px solid black;border-left: 2px solid black;border-right: 2px solid black;} #pre-head {border-right:2px solid black;text-align:center;border-bottom: 2px solid black;} #last-head {text-align:center;border-bottom: 2px solid black;} #pre-col {border-right:1px solid black; border-bottom:1px solid black} .Summary{min-width:320px;} .Supplier{min-width:120px;} .Service{min-w-width:120px;} #last-col {border-bottom:1px solid black}</style>';
 		//let pdfTitle = (this.state.facility ? this.state.facility.name+' ' : '')+'Status Report '+(this.state.startDate || this.state.endDate ?'for ('+(this.state.startDate? moment( this.state.startDate ).format('DD/MM/YY'):'')+' -'(this.state.endDate? moment( this.state.endDate ).format('DD/MM/YY'):'')+' )':'')
-		let pdfTitle = (this.state.facility ? this.state.facility.name+' ' : '')+'Status Report '+((this.state.startDate || this.state.endDate)?('for ('+(this.state.startDate ? moment( this.state.startDate ).format('DD/MM/YY'):'')+' - '+(this.state.endDate ? moment( this.state.endDate ).format('DD/MM/YY'):'')+')'):'')
-		let pdfName = (this.state.facility ? this.state.facility.name+'_' : '')+'Status Report '+((this.state.startDate || this.state.endDate)?('_('+(this.state.startDate ? moment( this.state.startDate ).format('DD-MM-YY'):'')+' to '+(this.state.endDate ? moment( this.state.endDate ).format('DD-MM-YY'):'')+')'):'')
+		let pdfTitle = (this.state.facility ? this.state.facility.name+' ' : 'All Facility ')+'Status Report '+((this.state.startDate || this.state.endDate)?('for ('+(this.state.startDate ? moment( this.state.startDate ).format('DD/MM/YY'):'')+' - '+(this.state.endDate ? moment( this.state.endDate ).format('DD/MM/YY'):'')+')'):'')
+		let pdfName = (this.state.facility ? this.state.facility.name+'_' : 'All Facility_')+'Status Report '+((this.state.startDate || this.state.endDate)?('_('+(this.state.startDate ? moment( this.state.startDate ).format('DD-MM-YY'):'')+' to '+(this.state.endDate ? moment( this.state.endDate ).format('DD-MM-YY'):'')+')'):'')
 		let pdfDetails = {
 			styleForPDF:styleForPDF,
 			pdfTitle:pdfTitle,
@@ -206,7 +214,7 @@ const RequestsStatusReport = React.createClass( {
 
 					<h2>Status Report</h2>
 	                {this.state.dataset ? <div>
-					<Menu items = { [ download(this.state.dataset, pdfDetails), print(this.state.dataset, this.refs.printable, pdfDetails) ] } />
+					<Menu items = { [ download(this.state.dataset), print(this.state.dataset, this.refs.printable, pdfDetails) ] } />
 				</div>:null}
 					<div className="row">
 						<div className="col-md-4">
@@ -232,10 +240,16 @@ const RequestsStatusReport = React.createClass( {
 								value       = { facility }
 								items       = { team ? team.getFacilities() : null }
 								onChange    = { ( facility ) => {
-									this.setState( {
+									let stateToSet = {
 										facility: facility,
 										showFacilityName: false
-									} ) } }
+									}
+									if(_.isEmpty(facility)){
+										stateToSet.showFacilityName = (facility && facility.name ? false : true);
+										stateToSet.service = null;
+									}
+									this.setState(stateToSet)
+								} }
 							/>
 
 						</div>
