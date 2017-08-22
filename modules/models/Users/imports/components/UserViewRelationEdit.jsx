c9aaa9da4ac6eca5027357476a75cf123d05cd8c
Norbert Glen <norbertglen7@gmail.com>
2017-08-22 14:13:33 +0300

    corrected updates pushed to wrong feature branch

--- modules/models/Users/imports/components/UserViewRelationEdit.jsx
+++ modules/models/Users/imports/components/UserViewRelationEdit.jsx
@@ -78,7 +78,7 @@ export default UserViewRelationEdit = React.createClass( {
 					placeholder 	= "Role"
 				/>
 
-				{_.contains(['portfolio manager','fmc support'], userRole) && role=='manager' && group && group.type != 'contractor'?
+				{_.contains(['portfolio manager','fmc support'], userRole) && role=='manager'?
 				<Text
 					value			= { threshold }
 					onChange		= { this.handleThresholdValueChange }
