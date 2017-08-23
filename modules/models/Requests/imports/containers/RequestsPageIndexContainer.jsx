84b49c8f3b8f9359eac52c8a4bd6f11da4da2252
Abhishek singh <abhishekex2011@gmail.com>
2017-08-23 16:17:42 +0530

    [FWA-1513]-Bookings not loading into 'Open' Requests Filter, should also be 'Complete' after time lapses

--- modules/models/Requests/imports/containers/RequestsPageIndexContainer.jsx
+++ modules/models/Requests/imports/containers/RequestsPageIndexContainer.jsx
@@ -47,7 +47,7 @@ export default RequestsPageIndexContainer = createContainer( ( { selectedRequest
 	}
 	else {
 		selectedStatus = 'Open';
-		statusFilter = { "status": { $in: [ 'New', 'Issued' ] } };
+		statusFilter = { "status": { $in: [ 'New', 'Issued', 'Booking' ] } };
 	}
 
 	if( includeComplete ) {
