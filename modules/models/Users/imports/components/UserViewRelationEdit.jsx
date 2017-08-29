5dda3b384e821f6cd2b963b25cd402b675b005e3
Paul Adrian Montano <pauladrian.montano@gmail.com>
2017-08-29 17:22:35 +0800

    [FWA-1521] - Restricted the addition of property managers in real estate agency type properties

--- modules/models/Users/imports/components/UserViewRelationEdit.jsx
+++ modules/models/Users/imports/components/UserViewRelationEdit.jsx
@@ -4,95 +4,99 @@
  */
 
 import React from "react";
-import { Select, Text } from '/modules/ui/MaterialInputs';
+import {Select, Text} from '/modules/ui/MaterialInputs';
 
-import { Users } from '/modules/models/Users';
+import {Users} from '/modules/models/Users';
 
 // so this should perhaps be included in the docmembers package??
-export default UserViewRelationEdit = React.createClass( {
+export default UserViewRelationEdit = React.createClass({
 
-	handleRoleChange( role ) {
-		var member, group;
-		member = this.props.member;
-		group = this.props.group;
-		group.setMemberRole( member, role );
-		this.handleThresholdValueChange(null);
-		
-		if ( !_.contains([ "portfolio manager" ], role) ) {
-			Users.update( { _id: member._id}, { $set: { role: role } } );
-			this.props.team.setMemberRole( member, role );
-			if (this.props.onChange) {
-				this.props.onChange();
-			}
-		}
-	},
+  handleRoleChange(role) {
+    let member, group;
+    member = this.props.member;
+    group = this.props.group;
+    group.setMemberRole(member, role);
+    this.handleThresholdValueChange(null);
 
-	handleThresholdValueChange( threshold ) {
-		var member, group;
-		member = this.props.member;
-		group = this.props.group;
-		group.setMemberThresholdValue( member, threshold );
-	},
+    if (!_.contains(["portfolio manager"], role)) {
+      Users.update({_id: member._id}, {$set: {role: role}});
+      this.props.team.setMemberRole(member, role);
+      if (this.props.onChange) {
+        this.props.onChange();
+      }
+    }
+  },
 
-	render() {
-		let { member, group } = this.props;
-		if ( !group || group.collectionName == "Requests" ) {
-			return <div/>
-		}
+  handleThresholdValueChange(threshold) {
+    var member, group;
+    member = this.props.member;
+    group = this.props.group;
+    group.setMemberThresholdValue(member, threshold);
+  },
 
-		let relation = group.getMemberRelation( member );
-		let userRole = Meteor.user().getRole();
+  render() {
+    let {member, group} = this.props;
+    if (!group || group.collectionName == "Requests") {
+      return <div/>
+    }
 
-		if( userRole == 'caretaker' ) {
-			roles = [
-				"staff",
-				"tenant",
-				"resident",
-				"property manager",
-			]
-		} else if ( userRole == "manager") {
-			roles = [ "staff", "manager", "support" ]
-		} else {
-			roles = [
-				"staff",
-				"tenant",
-				"support",
-				"manager",
-				"resident",
-				"caretaker",
-				"property manager",
-				"portfolio manager",
-			]
-			if( userRole == "fmc support" ) {
-				roles.push("fmc support");
-			}
-		}
-		if ( relation ) {
-			let role = relation.role,
-			threshold = relation.issueThresholdValue ? relation.issueThresholdValue : "",
-			currentUserRelation = group.getMemberRelation( Meteor.user() );
-			
-			return (
-				<div>
-				<Select
-					items 			= { roles }
-					value			= { role }
-					onChange		= { this.handleRoleChange }
-					placeholder 	= "Role"
-				/>
+    let relation = group.getMemberRelation(member);
+    let userRole = Meteor.user().getRole();
+    let roles = [];
+    if (userRole === 'caretaker') {
+      roles = [
+        "staff",
+        "tenant",
+        "resident",
+      ];
+      if (group.type === 'real estate') {
+        roles.push("property manager");
+      }
+    } else if (userRole == "manager") {
+      roles = ["staff", "manager", "support"]
+    } else {
+      roles = [
+        "staff",
+        "tenant",
+        "support",
+        "manager",
+        "resident",
+        "caretaker",
+        "portfolio manager",
+      ];
+      if (group.type === 'real estate') {
+        roles.push("property manager");
+      }
+      if (userRole == "fmc support") {
+        roles.push("fmc support");
+      }
+    }
+    if (relation) {
+      let role = relation.role,
+        threshold = relation.issueThresholdValue ? relation.issueThresholdValue : "",
+        currentUserRelation = group.getMemberRelation(Meteor.user());
 
-				{(_.contains(['portfolio manager','fmc support'], userRole) ) && 
-				_.contains(['manager','caretaker'], role) && group && group.type != 'contractor'? 
-				<Text
-					value			= { threshold }
-					onChange		= { this.handleThresholdValueChange }
-					placeholder 	= "Threshold WO Value for managers"
-				/>: null}
-				</div>
-			)
+      return (
+        <div>
+          <Select
+            items={ roles }
+            value={ role }
+            onChange={ this.handleRoleChange }
+            placeholder="Role"
+          />
 
-		}else{
-			return <div></div>
-		}
-	}
-} );
+          {(_.contains(['portfolio manager', 'fmc support'], userRole) ) &&
+          _.contains(['manager', 'caretaker'], role) && group && group.type != 'contractor' ?
+            <Text
+              value={ threshold }
+              onChange={ this.handleThresholdValueChange }
+              placeholder="Threshold WO Value for managers"
+            /> : null}
+        </div>
+      )
+
+    } else {
+      return <div></div>
+    }
+  }
+});
