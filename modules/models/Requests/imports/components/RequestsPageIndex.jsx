82ab2d66596a6433597335a72176556c240f4f27
Paul Adrian Montano <pauladrian.montano@gmail.com>
2017-08-30 01:54:39 +0800

    [FWA-1500] - fixed issue with duplicate WO and missing due dates.

--- modules/models/Requests/imports/components/RequestsPageIndex.jsx
+++ modules/models/Requests/imports/components/RequestsPageIndex.jsx
@@ -1,108 +1,111 @@
 /**
- * @author 			Leo Keith <leo@fmclarity.com>
- * @copyright 		2016 FM Clarity Pty Ltd.
+ * @author      Leo Keith <leo@fmclarity.com>
+ * @copyright    2016 FM Clarity Pty Ltd.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { FacilityFilter } from '/modules/models/Facilities';
-import { RequestActions, RequestsTable } from '/modules/models/Requests';
+import {FacilityFilter} from '/modules/models/Facilities';
+import {RequestActions, RequestsTable} from '/modules/models/Requests';
 
-import { RequestFilter } from '/modules/models/Requests';
+import {RequestFilter} from '/modules/models/Requests';
 import RequestPagination from './RequestPagination';
 
-import { Switch } from '/modules/ui/MaterialInputs';
+import {Switch} from '/modules/ui/MaterialInputs';
 import moment from 'moment';
 import Perf from 'react-addons-perf';
 
 /**
- * @class 			RequestsPageIndex
- * @memberOf 		module:models/Requests
+ * @class      RequestsPageIndex
+ * @memberOf    module:models/Requests
  */
 
 export default class RequestsPageIndex extends Component {
 
-	constructor(props){
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			requests: props.requests,
-      totalCollectionCount: props.totalCollectionCount,
-      pageSize: props.pageSize,
-			currentPage: props.currentPage,
-			active: false,
-		};
-
-	}
-
-	componentWillReceiveProps( props ){
-		this.props = props;
-
-		this.setState( {
-			requests: props.requests,
+    this.state = {
+      requests: props.requests,
       totalCollectionCount: props.totalCollectionCount,
       pageSize: props.pageSize,
       currentPage: props.currentPage,
-		} )
-	}
+      active: false,
+    };
 
-	componentWillMount() {
-		//Perf.start();
-	}
+  }
 
-	componentDidMount() {
-		/*
-	    Perf.stop();
-	    console.log('output requests page load time');
-	    Perf.printInclusive();
-	    */
-	    // Perf.printWasted();
-	}
+  componentWillReceiveProps(props) {
+    this.props = props;
 
-	render() {
-		let { team, facility, facilities, requests, selectedRequest, selectedStatus } = this.props;
-		let user = Meteor.user();
-		if( !team ) {
-			return <div/>
-		}
+    this.setState({
+      requests: props.requests,
+      totalCollectionCount: props.totalCollectionCount,
+      pageSize: props.pageSize,
+      currentPage: props.currentPage,
+    })
+  }
 
-		if( selectedRequest ) {
-			RequestActions.view.run( selectedRequest );
-		}
+  componentWillMount() {
+    //Perf.start();
+  }
 
-		return (
-			<div>
-				<div className = "row">
-					<div className="col-xs-3">
-						<FacilityFilter items = { facilities } selectedItem = { facility } />
-					</div>
-					<div className="col-xs-offset-3 col-xs-3 desktop-only">
-						<RequestFilter items = { [ 'Open','Preventative','All', 'New','Booking', 'Issued', 'Complete', /*'Close',*/ 'Cancelled' ] } selectedItem = { selectedStatus } />
-					</div>
-					{ /*user.getRole && user.getRole() == 'fmc support' ?
-						<div className="col-xs-offset-9 col-xs-3" >
-							<Switch
-								value={ this.state.active}
-								onChange={ ( active ) => {
-									if( active ) {
-										let now  = new Date(),
-											requests = _.filter( this.state.requests, (r) => moment( r.dueDate ).isBefore( now ) );
-										this.setState( { active: active, requests: requests } );
-									} else {
-											this.setState( { active: active, requests: this.props.requests } );
-									}
-								}}>
-								Show Overdue Work Order only
-							</Switch>
-						</div> : null
-					*/ }
-				</div>
-				<div className = "issue-page animated fadeIn" style = { {paddingTop:"50px"} }>
-					<div className = "ibox">
-						<RequestsTable requests={ this.state.requests } selectedItem={selectedStatus} />
-					</div>
-					<RequestPagination totalCollectionCount={ this.state.totalCollectionCount } itemsPerPage={ this.state.pageSize } currentPage={ this.state.currentPage }/>
-				</div>
-			</div>
-		)
-	}
+  componentDidMount() {
+    /*
+     Perf.stop();
+     console.log('output requests page load time');
+     Perf.printInclusive();
+     */
+    // Perf.printWasted();
+  }
+
+  render() {
+    let {team, facility, facilities, requests, selectedRequest, selectedStatus} = this.props;
+    let user = Meteor.user();
+    if (!team) {
+      return <div/>
+    }
+
+    if (selectedRequest) {
+      RequestActions.view.run(selectedRequest);
+    }
+
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-3">
+            <FacilityFilter items={ facilities } selectedItem={ facility }/>
+          </div>
+          <div className="col-xs-offset-3 col-xs-3 desktop-only">
+            <RequestFilter
+              items={ ['Open', 'Preventative', 'All', 'New', 'Booking', 'Issued', 'Complete', /*'Close',*/ 'Cancelled'] }
+              selectedItem={ selectedStatus }/>
+          </div>
+          { /*user.getRole && user.getRole() == 'fmc support' ?
+           <div className="col-xs-offset-9 col-xs-3" >
+           <Switch
+           value={ this.state.active}
+           onChange={ ( active ) => {
+           if( active ) {
+           let now  = new Date(),
+           requests = _.filter( this.state.requests, (r) => moment( r.dueDate ).isBefore( now ) );
+           this.setState( { active: active, requests: requests } );
+           } else {
+           this.setState( { active: active, requests: this.props.requests } );
+           }
+           }}>
+           Show Overdue Work Order only
+           </Switch>
+           </div> : null
+           */ }
+        </div>
+        <div className="issue-page animated fadeIn" style={ {paddingTop: "50px"} }>
+          <div className="ibox">
+            <RequestsTable requests={ this.state.requests } selectedItem={selectedStatus}/>
+          </div>
+          <RequestPagination totalCollectionCount={ this.state.totalCollectionCount }
+                             itemsPerPage={ this.state.pageSize } currentPage={ this.state.currentPage }/>
+        </div>
+      </div>
+    )
+  }
 }
--- modules/models/Requests/imports/containers/RequestsPageIndexContainer.jsx
+++ modules/models/Requests/imports/containers/RequestsPageIndexContainer.jsx
@@ -1,104 +1,105 @@
 import React from "react";
-import { createContainer } from 'meteor/react-meteor-data';
+import {createContainer} from 'meteor/react-meteor-data';
 import RequestsPageIndex from '../components/RequestsPageIndex.jsx';
-import { Facilities } from '/modules/models/Facilities';
-import { Requests } from '/modules/models/Requests';
+import {Facilities} from '/modules/models/Facilities';
+import {Requests} from '/modules/models/Requests';
 
-export default RequestsPageIndexContainer = createContainer( ( { selectedRequestId } ) => {
+export default RequestsPageIndexContainer = createContainer(({selectedRequestId}) => {
 
-	let facility = Session.getSelectedFacility(),
-		selectedStatus = Session.get('selectedStatus'),
-		team = Session.getSelectedTeam(),
-		user = Meteor.user(),
-		requests = null,
-		facilities = null,
-		statusFilter = null,
-		contextFilter = {},
-		selectedRequest = null,
-		includeComplete = false,
-  	totalCollectionCount = 0,
-		pageSize = 25,
-	  currentPage = Session.get('currentRequestPageNumber') > 0 ? Session.get('currentRequestPageNumber') : 0;
+  let facility = Session.getSelectedFacility(),
+    selectedStatus = Session.get('selectedStatus'),
+    team = Session.getSelectedTeam(),
+    user = Meteor.user(),
+    requests = null,
+    facilities = null,
+    statusFilter = null,
+    contextFilter = {},
+    selectedRequest = null,
+    includeComplete = false,
+    totalCollectionCount = 0,
+    pageSize = 25,
+    currentPage = Session.get('currentRequestPageNumber') > 0 ? Session.get('currentRequestPageNumber') : 0;
 
-	if ( selectedStatus == 'New' ) {
-		statusFilter = { "status": 'New' };
-	}
-	else if ( selectedStatus == 'Issued' ) {
-		statusFilter = { "status": 'Issued' };
-	}
-	else if ( selectedStatus == 'Complete' ) {
-		statusFilter = { "status": 'Complete' };
-		includeComplete = true;
-	}
-	else if ( selectedStatus == 'Close' ) {
-		statusFilter = { "status": 'Close' };
-		includeComplete = true;
-	}
-	else if ( selectedStatus == 'Booking' ){
-		statusFilter = { "status": 'Booking' };
-	}
-	else if ( selectedStatus == 'Preventative' ){
-		statusFilter = {
-			"status": 'Issued',"type": 'Preventative' };
-	}
-	else if ( selectedStatus == 'Cancelled' ) {
-		statusFilter = { "status": 'Cancelled' };
-		includeComplete = true;
-	}
-	else if ( selectedStatus == 'All' ) {
-		statusFilter = { "status": { $in:[ 'Open','Booking', 'New', 'Issued', 'Complete', 'Close', 'Cancelled' ] }  };
-		includeComplete = true;
-	}
-	else {
-		selectedStatus = 'Open';
-		statusFilter = { "status": { $in: [ 'New', 'Issued', 'Booking' ] } };
-	}
+  if (selectedStatus == 'New') {
+    statusFilter = {"status": 'New'};
+  }
+  else if (selectedStatus == 'Issued') {
+    statusFilter = {"status": 'Issued'};
+  }
+  else if (selectedStatus == 'Complete') {
+    statusFilter = {"status": 'Complete'};
+    includeComplete = true;
+  }
+  else if (selectedStatus == 'Close') {
+    statusFilter = {"status": 'Close'};
+    includeComplete = true;
+  }
+  else if (selectedStatus == 'Booking') {
+    statusFilter = {"status": 'Booking'};
+  }
+  else if (selectedStatus == 'Preventative') {
+    statusFilter = {
+      "status": 'Issued', "type": 'Preventative'
+    };
+  }
+  else if (selectedStatus == 'Cancelled') {
+    statusFilter = {"status": 'Cancelled'};
+    includeComplete = true;
+  }
+  else if (selectedStatus == 'All') {
+    statusFilter = {"status": {$in: ['Open', 'Booking', 'New', 'Issued', 'Complete', 'Close', 'Cancelled']}};
+    includeComplete = true;
+  }
+  else {
+    selectedStatus = 'Open';
+    statusFilter = {"status": {$in: ['New', 'Issued', 'Booking']}};
+  }
 
-	if( includeComplete ) {
-		Meteor.subscribe( 'Requests: Complete' );
-	}
-
-
-	if ( selectedRequestId ) {
-		selectedRequest = Requests.findOne( selectedRequestId );
-	}
-
-	if ( team ) {
-		//facilities = Facilities.findAll( { 'team._id': team._id } );
-		facilities = team.getFacilities();
-		if ( facilities ) {
-			let facilityThumbs = _.pluck( facilities, 'thumb' );
-			Meteor.subscribe( 'Thumbs', facilityThumbs );
-		}
-	}
-
-	if ( facility && facility._id ) {
-		contextFilter[ 'facility._id' ] = facility._id;
-	} else if ( team && team._id ) {
-		//contextFilter[ 'team._id' ] = team._id;
-	}
-
-	if ( user != null ) {
-	    // could test moving this below loading team and only including facilities if supplier
-		({ requests, totalCollectionCount, currentPage } = user.getRequests(
-			{ $and: [ statusFilter, contextFilter ] }, { expandPMP: true, skip: currentPage, limit: pageSize }
-		));
-		//requests = user.getRequests();
-	}
+  if (includeComplete) {
+    Meteor.subscribe('Requests: Complete');
+  }
 
 
-	return {
-		team,
-		facilities,
-		facility,
-		requests,
-		selectedStatus,
-		selectedRequest,
-		contextFilter,
-		statusFilter,
-		user,
+  if (selectedRequestId) {
+    selectedRequest = Requests.findOne(selectedRequestId);
+  }
+
+  if (team) {
+    //facilities = Facilities.findAll( { 'team._id': team._id } );
+    facilities = team.getFacilities();
+    if (facilities) {
+      let facilityThumbs = _.pluck(facilities, 'thumb');
+      Meteor.subscribe('Thumbs', facilityThumbs);
+    }
+  }
+
+  if (facility && facility._id) {
+    contextFilter['facility._id'] = facility._id;
+  } else if (team && team._id) {
+    //contextFilter[ 'team._id' ] = team._id;
+  }
+
+  if (user != null) {
+    // could test moving this below loading team and only including facilities if supplier
+    ({requests, totalCollectionCount, currentPage} = user.getRequests(
+      {$and: [statusFilter, contextFilter]}, {expandPMP: true, skip: currentPage, limit: pageSize}
+    ));
+    //requests = user.getRequests();
+  }
+
+
+  return {
+    team,
+    facilities,
+    facility,
+    requests,
+    selectedStatus,
+    selectedRequest,
+    contextFilter,
+    statusFilter,
+    user,
     totalCollectionCount,
-		currentPage,
+    currentPage,
     pageSize
-	}
-}, RequestsPageIndex );
+  }
+}, RequestsPageIndex);
