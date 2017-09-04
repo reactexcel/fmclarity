817072b660cad9a90ddc33ea1bd06cf87c29824e
Abhishek singh <abhishekex2011@gmail.com>
2017-09-04 15:21:30 +0530

    [FWA-1494]-Work acc. to new AC

--- modules/models/Facilities/imports/components/FacilityFilter.jsx
+++ modules/models/Facilities/imports/components/FacilityFilter.jsx
@@ -6,6 +6,7 @@ import React from "react";
 
 import { NavListDropDown } from '/modules/ui/MaterialNavigation';
 import FacilityListTile from './FacilityListTile.jsx';
+import { Roles } from '/modules/mixins/Roles';
 
 /**
  * An ui component that implements a drop down selection list for facilities.
@@ -17,10 +18,16 @@ import FacilityListTile from './FacilityListTile.jsx';
  * @copyright       2016 FM Clarity Pty Ltd.
  */
 function FacilityFilter( props ) {
+	let logedUser = Meteor.user();
+	let newFacilityList = [];
+	if(logedUser && !_.contains(['fmc support','portfolio manager'],logedUser.getRole())){
+		newFacilityList = Roles.getAssociateFacility( logedUser );
+	}
 	return (
 		<div style = { { position:"absolute", zIndex:1300 } }>
 		{props.items && props.items.length > 1 ? <NavListDropDown
 					{ ...props }
+					items = { newFacilityList && newFacilityList.length ? newFacilityList : props.items } 
 					ListTile = { FacilityListTile }
 					startOpen = { false }
 					onChange = { ( facility ) => {
--- modules/models/Facilities/imports/containers/FacilitiesPageIndexContainer.jsx
+++ modules/models/Facilities/imports/containers/FacilitiesPageIndexContainer.jsx
@@ -44,11 +44,13 @@ const FacilitiesPageIndexContainer = createContainer( ( params ) => {
 			Meteor.subscribe( 'Thumbs', facilityThumbs );
 		}
 	}
-	if(!_.contains(['fmc support'],Meteor.user().getRole())){
-        let logedUser = Meteor.user();
-        let newFacilityList = Roles.getAssociateFacility( logedUser );
-        facilities = newFacilityList
-    }
+	let logedUser = Meteor.user();
+	if(logedUser){
+		if(!_.contains(['fmc support','portfolio manager'],logedUser.getRole())){
+	        let newFacilityList = Roles.getAssociateFacility( logedUser );
+	        facilities = newFacilityList
+	    }
+	}
 	return {
 		team,
 		facility,
