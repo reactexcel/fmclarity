d9d80d45f04bee58d3c94ee26a29db9acad84fcf
Abhishek singh <abhishekex2011@gmail.com>
2017-09-26 13:59:39 +0530

    [FWA-1585]-[REQUESTS] Console error when clicking on contacts tab of Request

--- modules/models/Requests/imports/components/RequestPanel.jsx
+++ modules/models/Requests/imports/components/RequestPanel.jsx
@@ -528,6 +528,7 @@ const RequestPanelInner = ( { request, nextDate, previousDate, nextRequest, prev
                                     hideMenu    = { _.contains( [ 'staff', 'resident', 'tenant' ], Meteor.user().getRole() ) }
                                     group       = { request }
                                     readOnly    = { true }
+                                    team        = { Session.get( 'selectedTeam' ) }
                                 />
                 }
             ]} />
